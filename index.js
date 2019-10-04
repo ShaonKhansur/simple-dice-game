cc7cde306357a02a8fe22842f1e6840635c5e1c9
shaon <shaonkhansur@gmail.com>
2019-10-05 03:27:34 +0600

    successfully

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const numbers = [];
 function diceClick() {
   let randomNumber = Math.ceil(Math.random() * 6);
 
-  if(randomNumber !== 6) {
+  if (randomNumber !== 6) {
     console.log(randomNumber);
     numbers.push(randomNumber);
   } else {
-    console.log('Six');
+    console.log("Six");
     numbers.push(randomNumber);
     for (let i = 0; i <= numbers.length; i++) {
-      if(numbers[i] + numbers[i + 1] === 12) {
-        let score = numbers.reduce((a, b) => {return a + b});
-        alert('Lose!! 6 repeated! Your Score is ' + score);
+      if (numbers[i] + numbers[i + 1] === 12) {
+        let score = numbers.reduce((a, b) => {
+          return a + b;
+        });
+        alert("Lose!! 6 repeated! Your Score is " + score);
         console.log(numbers);
       }
     }
   }
 }
-
